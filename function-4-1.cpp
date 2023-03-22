f139ca91dacee7f3faa30a60b89ea3d83cbbace3
root <root@DESKTOP-CDDHE9C.localdomain>
2023-03-22 14:21:53 +1030

    ws04

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 
-extern int *readNumbers(int);
+extern int *readNumbers();
 extern int secondSmallestSum(int *,int);
 
-int *readNumbers(int length)
+int *readNumbers()
 {
     int *number_arr=new int[10];
-    for(int i=0;i<length;i++)
+    for(int i=0;i<10;i++)
     {
         int a=0;
         std::cin>>a;
@@ -18,13 +18,32 @@ int *readNumbers(int length)
 
 int secondSmallestSum(int *numbers,int length)
 {
-    int arr[length];
-    arr[0]=numbers[0];
+    int *arr=new int[100];
+   
+    int arr_id=0;
     for(int i=0;i<length;i++)
     {
-        for(int j=0;j<length;j++)
+        int sum=0;
+        for(int j=i;j<length;j++)
         {
-
+            sum+=numbers[j];
+            arr[arr_id]=sum;
+            arr_id++;
         }
     }
+    int a=0;
+    for(int i=0;i<100;i++)
+    {
+        for(int j=i;j<100;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                a=arr[j];
+                arr[j]=arr[i];
+                arr[i]=a;
+            }
+        }
+
+    }
+    return arr[1];
 }
