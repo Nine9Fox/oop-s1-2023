ce96ac2f31af0dc527b47e718fed5d1c87fd2776
root <root@DESKTOP-CDDHE9C.localdomain>
2023-03-23 22:15:28 +1030

    pra3

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -3,23 +3,21 @@
 using namespace std;
 
 extern PersonList deepCopyPersonList(PersonList);
-extern PersonList createPersonList(int); 
+//extern PersonList createPersonList(int); 
 
 int main()
 {
     int n=5;
-    PersonList a=createPersonList(n);
-    PersonList b=deepCopyPersonList(a);
-    for(int i=0;i<n;i++)
-    {
-        cout<<b.people[i].name<<" "<<b.people[i].age<<endl;
-    } 
-    cout<<"a "<<&a.people<<endl;
-    cout<<"b "<<&b.people<<endl;
-    a.people[1].name="x";
-    for(int i=0;i<n;i++)
-    {
-        cout<<b.people[i].name<<" "<<b.people[i].age<<endl;
-    } 
+    //for(int i=0;i<n;i++)
+    //{
+    //    cout<<b.people[i].name<<" "<<b.people[i].age<<endl;
+    //} 
+    //cout<<"a "<<&a.people<<endl;
+    //cout<<"b "<<&b.people<<endl;
+    //a.people[1].name="x";
+    //for(int i=0;i<n;i++)
+    //{
+    //    cout<<b.people[i].name<<" "<<b.people[i].age<<endl;
+    //}
     return 0;
 }
