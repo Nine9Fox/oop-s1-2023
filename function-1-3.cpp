578f7d95a30b9eead1de30d38213eabd6c8e21d9
root <root@DESKTOP-CDDHE9C.localdomain>
2023-03-23 22:09:24 +1030

    pra3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 PersonList deepCopyPersonList(PersonList pl)
 {
-    PersonList p_copy=pl;
+    PersonList p_copy;
+    p_copy.numPeople = pl.numPeople;
+    p_copy.people = new Person[pl.numPeople];
+    for (int i = 0; i < pl.numPeople; i++) {
+        p_copy.people[i].name = pl.people[i].name;
+        p_copy.people[i].age = pl.people[i].age;
+    }
     return p_copy;
 }
